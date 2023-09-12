ef0a4f76e7c5b37e14a4fb67307cf378bf4901a2
farhadcse7 <fmfarhad23@gmail.com>
2023-09-12 20:16:21 +0600

    input text filed updated

--- lib/main.dart
+++ lib/main.dart
@@ -19,36 +19,23 @@ class MyApp extends StatelessWidget{
 class HomeScreen extends StatelessWidget{
   const HomeScreen({super.key});
 
-  MySnackBar(message,context){
-    return ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
-  }
-  MyAlertDialog(context){
-    return showDialog(context: context, builder: (BuildContext context){
-      return Expanded(child: AlertDialog(
-        title: Text("Alert!"),
-        content: Text("do you want to delete"),
-        actions: [
-        TextButton(onPressed: (){MySnackBar("Delete Successful", context);}, child: Text("yes")),
-        TextButton(onPressed: (){Navigator.of(context).pop();}, child: Text("no")),],
-      ),);
-    });
-  }
-  
   @override
   Widget build(BuildContext context) {
     ButtonStyle buttonStyle=ElevatedButton.styleFrom(
-      padding: EdgeInsets.all(10), 
-      backgroundColor: Colors.pink,
-      foregroundColor: Colors.black,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(40))),
+      minimumSize: Size(double.infinity, 60),
       );
+
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.red, title: const Text('home'), ),
-      body: Center(child: ElevatedButton(child: Text("click me"), onPressed: () {
-        MyAlertDialog(context);
-      },),
-        
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+        Padding(padding: EdgeInsets.all(10), child: TextField(decoration: InputDecoration(labelText: 'First Name', border: OutlineInputBorder()),),),
+        Padding(padding: EdgeInsets.all(10), child: TextField(decoration: InputDecoration(labelText: 'First Name', border: OutlineInputBorder()),),),
+        Padding(padding: EdgeInsets.all(10), child: TextField(decoration: InputDecoration(labelText: 'First Name', border: OutlineInputBorder()),),),
+        Padding(padding: EdgeInsets.all(10), child: ElevatedButton(onPressed: () {
+        },child: Text('Submit'), style: buttonStyle,),),
+        ],
       )
     );
   }
